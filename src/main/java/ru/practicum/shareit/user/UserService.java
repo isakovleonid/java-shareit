a558657bf636c38065409178a485d710a4d4861d
Isakov Leonid <isakov.leonid@gmail.com>
2025-06-06 13:29:57 +0300

    Спринт 15 Исправление обновления User на builder

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.handler.IncorrectDataException;
 import ru.practicum.shareit.handler.NotFoundException;
-import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 import java.util.Optional;
