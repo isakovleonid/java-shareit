2e5d3b2bc940543ac69632e80dc04bfc0490c4e6
Isakov Leonid <isakov.leonid@gmail.com>
2025-06-16 10:45:22 +0300

    Спринт 16 Доработка тестов

--- server/src/test/java/ru/practicum/shareit/UserControllerIntegrationTest.java
+++ server/src/test/java/ru/practicum/shareit/UserControllerIntegrationTest.java
@@ -163,22 +163,4 @@ public class UserControllerIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
-
-    @Test
-    void addUser_shouldReturnConflict_whenEmailAlreadyExists() throws Exception {
-        User oldUser = User.builder()
-                .name("Тестовый пользователь")
-                .email("test@example.com")
-                .build();
-        oldUser = userRepository.save(oldUser);
-
-        UserDto duplicateUser = new UserDto();
-        duplicateUser.setName("Дубликат email");
-        duplicateUser.setEmail(oldUser.getEmail());
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(duplicateUser)))
-                .andExpect(status().isConflict());
-    }
 }
