2762fb8972f38be29d784c060d2eee9bd7c89b04
Isakov Leonid <isakov.leonid@gmail.com>
2025-06-18 23:24:52 +0300

    Спринт 16 Доработаны тесты

--- server/src/test/java/ru/practicum/shareit/ItemControllerIntegrationTest.java
+++ server/src/test/java/ru/practicum/shareit/ItemControllerIntegrationTest.java
@@ -22,7 +22,6 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
--- server/src/test/java/ru/practicum/shareit/UserControllerIntegrationTest.java
+++ server/src/test/java/ru/practicum/shareit/UserControllerIntegrationTest.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
